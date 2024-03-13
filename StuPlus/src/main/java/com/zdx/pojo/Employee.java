b3e6e9a4f4071cb284a73a05cf287ce72198e011
zdx-1 <19546328912@163.com>
2024-03-13 18:31:22 +0800

    测试

---
+++ StuPlus/src/main/java/com/zdx/pojo/Employee.java
@@ -0,0 +1,63 @@
+package com.zdx.pojo;
+
+public class Employee {
+
+
+    private int id;
+    private String name;
+    private int age;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public Employee(int id, String name, int age, String position) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+    public Employee() {
+    }
+    //构造方法
+    //重写toString方法
+}
---
+++ StuPlus/src/main/java/com/zdx/sesrvice/test.java
@@ -0,0 +1,117 @@
+package com.zdx.sesrvice;
+
+import com.zdx.pojo.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.Scanner;
+
+public class test {
+    static SqlSession sqlSession=null;
+
+    public static void main(String[] args) {
+//        读取配置文件，创建SqlSession，初始化SqlSession
+        String resources ="mybatis-config.xml";
+        Reader reader=null;
+        try {
+            reader= Resources.getResourceAsReader(resources);
+            SqlSessionFactory sqlMapper=new SqlSessionFactoryBuilder().build(reader);
+            sqlSession=sqlMapper.openSession();
+            System.out.println("---欢迎使用本系统！---");
+            while (true){
+                System.out.println("1.查询员工信息");
+                System.out.println("2.添加员工信息");
+                System.out.println("3.修改员工信息");
+                System.out.println("4.删除员工信息");
+                System.out.println("5.退出系统");
+                System.out.print("请输入您的选项：");
+                Scanner scanner=new Scanner(System.in);
+                int i=scanner.nextInt();
+                switch (i){
+                    case 1:
+                        System.out.println("查询所有员工信息还是根据id查询员工信息？");
+                        System.out.println("请输入您的选项：");
+                        System.out.println("1.查询员工信息");
+                        System.out.println("2.查询所有员工信息");
+                        int j=scanner.nextInt();
+                        if (j==1){
+                            System.out.print("请输入员工id：");
+                            int id=scanner.nextInt();
+                            Employee employee=sqlSession.selectOne("com.zdx.mapper.EmployeeMapper.findById",id);
+                            System.out.println(employee);
+                        }else {
+                            List<Employee> list=sqlSession.selectList("com.zdx.mapper.EmployeeMapper.findAll");
+                            for (Employee employee:list){
+                                System.out.println(employee);
+                            }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("添加员工信息");
+                        System.out.print("请输入员工姓名：");
+                        String name=scanner.next();
+                        System.out.print("请输入员工年龄：");
+                        int age=scanner.nextInt();
+                        System.out.print("请输入员工职位：");
+                        String position=scanner.next();
+                        Employee employee = new Employee(1, name, age, position);
+                        sqlSession.insert("com.zdx.mapper.EmployeeMapper.insert",employee);
+                        sqlSession.commit();
+                        System.out.println("添加成功");
+                        break;
+                    case 3:
+                        System.out.println("修改员工信息");
+                        System.out.print("请输入员工编号：");
+                        int id=scanner.nextInt();
+                        System.out.print("请输入员工姓名：");
+                        String name1=scanner.next();
+                        System.out.print("请输入员工年龄：");
+                        int age1=scanner.nextInt();
+                        System.out.print("请输入员工职位：");
+                        String position1=scanner.next();
+                        Employee employee1 = new Employee(id, name1, age1, position1);
+                        sqlSession.update("com.zdx.mapper.EmployeeMapper.updateById",employee1);
+                        sqlSession.commit();
+                        System.out.println("修改成功");
+                        break;
+                    case 4:
+                        System.out.println("删除员工信息");
+                        System.out.print("请输入员工编号：");
+                        int id1=scanner.nextInt();
+                        sqlSession.delete("com.zdx.mapper.EmployeeMapper.deleteById",id1);
+                        sqlSession.commit();
+                        System.out.println("删除成功");
+                        break;
+                    case 5:
+                        System.out.println("退出系统");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("输入有误，请重新输入！");
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
